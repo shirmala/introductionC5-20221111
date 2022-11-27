73066b28fe27434495c138282a69ecc3653b00a9
Timofeev <t9944m@yandex.ru>
2022-11-27 08:56:14 +0300

    Init

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
+int a;
+a=10;
+System.Console.Write ("Введите число");
+string? s=Console.ReadLine ();
+a=Convert.ToInt32 (s);
+int b=a*a;
+System.Console.WriteLine (b);
+System.Console.WriteLine ($"{a}^2={b}");
+
